925c10aa130a070914cdda5c0ace5f261acfe3b2
harukee-dev <hdjemsmc@gmail.com>
2025-01-02 03:11:21 -0800

    added title in home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,7 +4,8 @@ import classes from "./HomePage.module.css"
 export const HomePage = () => {
     return (
         <div className={classes.homePage}>
-            <h1 className={classes.testText}>Home Page</h1>
+            <h1 className={classes.bmwBackgroundTitle}>BMW</h1>
+            <h1 className={classes.bmwMainTitle}>BMW <br/> M8 2018</h1>
         </div>
     )
 }
