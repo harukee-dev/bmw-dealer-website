2ce71593bca523634cf188e477c6edcae0a70d73
harukee-dev <hdjemsmc@gmail.com>
2025-01-02 03:16:53 -0800

    added dots image in home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import classes from "./HomePage.module.css"
+import dotsImage from "../../images/dots-under-bmw-title.png"
 
 export const HomePage = () => {
     return (
         <div className={classes.homePage}>
             <h1 className={classes.bmwBackgroundTitle}>BMW</h1>
             <h1 className={classes.bmwMainTitle}>BMW <br/> M8 2018</h1>
+            <img className={classes.dotsImage} src={dotsImage} alt="dots-image" />
         </div>
     )
 }
