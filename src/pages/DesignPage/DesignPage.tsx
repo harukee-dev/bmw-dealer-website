42dfdc3ba143b1e9d402348e72867cf8fdea23b2
harukee-dev <hdjemsmc@gmail.com>
2025-01-03 21:36:49 -0800

    test commit

--- src/pages/DesignPage/DesignPage.tsx
+++ src/pages/DesignPage/DesignPage.tsx
@@ -4,7 +4,7 @@ import classes from "./DesignPage.module.css"
 export const DesignPage = () => {
     return (
         <div className={classes.designPage}>
-            <h1 className={classes.testText}>Design Page</h1>
+            <h1 className={classes.testText}>Design Page test</h1>
         </div>
     )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,7 +8,6 @@ export const HomePage = () => {
     return (
         <div className={classes.homePage}>
             <h1 className={classes.bmwBackgroundTitle}>BMW</h1>
-            {/* <h1 className={classes.bmwMainTitle}>BMW <br/> M8 2018</h1> */}
             <div>
                 <h1 className={classes.bmwMainFirstTitle}>BMW</h1>
                 <div className={classes.mainTitleSecondStroke}>
